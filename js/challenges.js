48b9aaefadfa1f477e291a0a40fc87085b457911
Daniel Palmares <65691696+danielpalmares@users.noreply.github.com>
2020-08-06 03:08:07 -0300

    Update challenges.js

--- js/challenges.js
+++ js/challenges.js
@@ -1,12 +1,3 @@
-/*
-YOUR 3 CHALLENGES
-Change the game to follow these rules:
-
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
-3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
-*/
-
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
@@ -19,34 +10,34 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         var dice1 = Math.floor(Math.random() * 6) + 1;
         var dice2 = Math.floor(Math.random() * 6) + 1;
 
-        //2. Display the result
+        // 2. Display the result
         document.getElementById('dice-1').style.display = 'block';
         document.getElementById('dice-2').style.display = 'block';
         document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
         document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
-        //3. Update the round score IF the rolled number was NOT a 1
+        // 3. Update the round score IF the rolled number was NOT a 1
         if (dice1 !== 1 && dice2 !== 1) {
-            //Add score
+            // Add score
             roundScore += dice1 + dice2;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
-            //Next player
+            // Next player
             nextPlayer();
         }
         
         /*
         if (dice === 6 && lastDice === 6) {
-            //Player looses score
+            // Player looses score
             scores[activePlayer] = 0;
             document.querySelector('#score-' + activePlayer).textContent = '0';
             nextPlayer();
         } else if (dice !== 1) {
-            //Add score
+            // Add score
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
-            //Next player
+            // Next player
             nextPlayer();
         }
         lastDice = dice;
@@ -83,7 +74,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
         } else {
-            //Next player
+            // Next player
             nextPlayer();
         }
     }
@@ -91,7 +82,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
 
 function nextPlayer() {
-    //Next player
+    // Next player
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
@@ -101,8 +92,8 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    //document.querySelector('.player-0-panel').classList.remove('active');
-    //document.querySelector('.player-1-panel').classList.add('active');
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
 
     document.getElementById('dice-1').style.display = 'none';
     document.getElementById('dice-2').style.display = 'none';
